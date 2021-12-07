045d214a68f411cc75dc89cea8047c8058b6e85a
DusanDjordjic <dukidjordjic@gmail.com>
2021-12-07 10:20:27 +0100

    Dropdown poboljsan i mali redesign

--- cool-projects/src/app/directives/open-dropdown.directive.ts
+++ cool-projects/src/app/directives/open-dropdown.directive.ts
@@ -4,12 +4,22 @@ import { Directive, HostListener, Input, OnInit } from '@angular/core';
   selector: '[openDropdown]',
 })
 export class OpenDropdownDirective implements OnInit {
+  timeout!: NodeJS.Timeout;
   @Input() dropdown = '';
   constructor() {}
-  @HostListener('click')
-  onClick() {
+  @HostListener('mouseenter')
+  onEnter() {
     const targetElement = document.querySelector(this.dropdown);
-    targetElement?.classList.toggle('active');
+    targetElement?.classList.add('active');
+    clearTimeout(this.timeout);
+  }
+  @HostListener('mouseleave')
+  onLeave(event: any) {
+    console.log('leave');
+    this.timeout = setTimeout(() => {
+      const targetElement = document.querySelector(this.dropdown);
+      targetElement?.classList.remove('active');
+    }, 250);
   }
   ngOnInit() {}
 }
